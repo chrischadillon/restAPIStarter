288c3464e3cca3fac34ebb0e9efffc1932075575
Chris Chadillon <chris@chadillon.com>
2017-03-16 16:57:10 -0400

    Starter

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,9 +2,9 @@ import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
 
-let names = ["usernames":[
-    ["first":"Chris", "Last":"Chadillon", "username":"cbcdiver", "password":"pencil99"],
-    ["first":"Alice", "Last":"Chadillon", "username":"dogs", "password":["pencil99","groundhog"]]
+let names = ["accounts":[
+    ["username":"cbcdiver", "email":"c@d.com", "password":"pencil99"],
+    ["username":"dogs", "email":"a@b.com", "password":"pencil99"]
 ]]
 
 let server = HTTPServer()
@@ -12,34 +12,6 @@ server.serverPort = 8080
 server.documentRoot = "webroot"
 
 var routes = Routes()
-
-routes.add(method: .get, uri: "/hello", handler: {
-    request, response in
-    response.appendBody(string: "<H1>Hello</H1>")
-    response.completed()
-})
-
-routes.add(method: .get, uri: "/json/all_data") {
-    request, response in
-    do {
-        try response.setBody(json: names)
-            response.setHeader(.contentType, value: "application/json")
-            response.completed()
-        
-    } catch {
-        response.setBody(string: "Error handling request: \(error)")
-        response.completed()
-    }
-}
-
-routes.add(method: .get, uri: "/json/username/{username}", handler: {
-    request, response in
-    
-    if let userName = request.urlVariables["username"] {
-        response.appendBody(string: userName)
-    }
-    response.completed()
-})
 
 server.addRoutes(routes)
 
